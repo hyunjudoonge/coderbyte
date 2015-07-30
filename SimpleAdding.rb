41ad961a25ba208beeed9fd1147e17bfbe18246c
hyunjudoonge <bsgtg3@hanmail.net>
2015-07-30 19:40:26 +0900

    Create SimpleAdding.rb

---
+++ SimpleAdding.rb
@@ -0,0 +1,13 @@
+def SimpleAdding(num)
+
+  # code goes here
+  
+  
+  egg = (1..num).inject(:+)
+  
+  return egg
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SimpleAdding(STDIN.gets)  
