1cc3eea94522b6cdd9b8ab9cfbdc2f1c79460cf3
hyunjudoonge <bsgtg3@hanmail.net>
2015-07-30 19:36:52 +0900

    Create FirstFactorial.rb

---
+++ FirstFactorial.rb
@@ -0,0 +1,12 @@
+def FirstFactorial(num)
+
+  # code goes here
+  egg = (1..num).inject(:*)
+  return egg
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+FirstFactorial(STDIN.gets)  
+
